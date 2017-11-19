2c3ada1a3bbb1f014ce64e51b5979e2fbc6d2103
Kazato Sugimoto <uiureo@gmail.com>
2017-11-19 16:57:14 +0900

    Add run command to capture

--- main.go
+++ main.go
@@ -2,47 +2,127 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/urfave/cli"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("pass args\n")
-		os.Exit(1)
+	app := cli.NewApp()
+	app.Name = "lonely"
+	app.Usage = "lonely client"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "run",
+			Usage: "Captures a current image and uploads it",
+			Action: func(c *cli.Context) error {
+				err := checkEnv()
+				if err != nil {
+					return err
+				}
+
+				dir, err := ioutil.TempDir("", "lonely-")
+				if err != nil {
+					return cli.NewExitError(err.Error(), 1)
+				}
+				defer os.RemoveAll(dir)
+
+				imagePath := dir + "/snapshot.jpg"
+
+				err = capture(imagePath)
+				if err != nil {
+					return err
+				}
+
+				err = upload(imagePath)
+
+				return err
+			},
+		},
+		{
+			Name:      "upload",
+			Usage:     "Uploads an image",
+			ArgsUsage: "[image_path]",
+			Action: func(c *cli.Context) error {
+				err := checkEnv()
+				if err != nil {
+					return err
+				}
+
+				err = upload(c.Args().First())
+				return nil
+			},
+		},
 	}
 
-	imagePath := os.Args[1]
+	app.Run(os.Args)
+}
 
+func checkEnv() error {
 	token := os.Getenv("LONELY_DEVICE_TOKEN")
 	if token == "" {
-		fmt.Println("$LONELY_DEVICE_TOKEN is required")
-		os.Exit(1)
+		return cli.NewExitError("$LONELY_DEVICE_TOKEN is required", 1)
 	}
 
 	host := os.Getenv("LONELY_SERVER_HOST")
 	if host == "" {
-		fmt.Println("$LONELY_SERVER_HOST is required")
-		os.Exit(1)
+		return cli.NewExitError("$LONELY_DEVICE_HOST is required", 1)
 	}
+
+	return nil
+}
+
+// Upload an image to lonely server
+func upload(imagePath string) error {
+	token := os.Getenv("LONELY_DEVICE_TOKEN")
+	host := os.Getenv("LONELY_SERVER_HOST")
 
 	apiClient := &ApiClient{Host: host, Token: token}
 
 	uploadResponse, err := apiClient.createUpload()
 	if err != nil {
-		fmt.Printf("error: during a request to create upload: %v \n", err)
-		os.Exit(1)
+		return cli.NewExitError(fmt.Sprintf("error: during a request to create upload: %v \n", err), 1)
 	}
 
 	err = apiClient.uploadImageToS3(uploadResponse.PresignedUrl, imagePath)
 	if err != nil {
-		fmt.Printf("error: during a request to upload image to s3: %v \n", err)
-		os.Exit(1)
+		return cli.NewExitError(fmt.Sprintf("error: during a request to upload image to s3: %v \n", err), 1)
 	}
 
 	err = apiClient.createImage(uploadResponse.Timestamp)
 
 	if err != nil {
-		fmt.Printf("error: during a request to register image: %v", err)
-		os.Exit(1)
+		return cli.NewExitError(fmt.Sprintf("error: during a request to register image: %v", err), 1)
 	}
+
+	return nil
+}
+
+// Capture an image from webcam
+//
+// requires following commands:
+//   linux: `fswebcam`
+//   mac:   `imagesnap`
+func capture(imagePath string) error {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "linux":
+		cmd = exec.Command("fswebcam", "--resolution", "1280x720", imagePath)
+	case "darwin":
+		cmd = exec.Command("imagesnap", "-w", "3", imagePath)
+	}
+
+	output, err := cmd.CombinedOutput()
+	fmt.Println(string(output))
+
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	return nil
 }
